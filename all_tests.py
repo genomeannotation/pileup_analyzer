7d76b002a45c801283b2ed0af542d438499d27cb
Brian Hall <bhall7@hawaii.edu>
2013-09-23 11:08:47 -1000

    workin on PileSanitizer...

--- all_tests.py
+++ all_tests.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
 import unittest
-from pileup_parser_classes import ConsensusCaller
+from pileup_parser_classes import ConsensusCaller, PileSanitizer
+
+class TestPileSanitizer(unittest.TestCase):
+    def test_sanitize(self):
+        sanitizer = PileSanitizer()
+        pile1 = 'AAAA+3CCCGG'
+        result1 = sanitizer.sanitize(pile1)
+        self.assertEqual('AAAAGG', result1)
 
 class TestConsensusCaller(unittest.TestCase):
     def test_initialize(self):
         caller = ConsensusCaller(depth= 10, frequency = .99)
         self.assertEqual(10, caller.min_depth_of_coverage)
 
-
+    def test_call(self):
+        caller = ConsensusCaller(depth = 5, frequency = .9)
+        test_pile1 = 'AAAAAAAAAAAAAAACA'
+        test_pile2 = 'AAAAAAAAAAAAAAAAAAA'
+        pile = [test_pile1, test_pile2]
+        result = caller.call(pile)
+        self.assertEqual('A', result)
 
 ##################################
 if __name__ == '__main__':
--- pileup_parser_classes.py
+++ pileup_parser_classes.py
@@ -1,7 +1,61 @@
 #!/usr/bin/env python
 
+import re
+
+class PileSanitizer:
+    def __init__(self):
+        self.indel_re = re.compile('[\+\-]')
+        self.cigar_re = re.compile('[\^\$]')
+
+    def sanitize(self, pile):
+        clean_pile = ''
+        skip = 0
+        on_indel = False
+        for base in pile:
+            m1 = self.indel_re.match(base)
+            if m1:
+                print("foo")
+                on_indel = True
+                continue
+            m2 = self.cigar_re.match(base)
+            if m2:
+                skip = 1
+                continue
+            if on_indel:
+                skip = int(base)
+                on_indel = False
+                continue
+            if skip > 0:
+                skip -= 1
+                continue
+            clean_pile += base
+        return clean_pile
+
 class ConsensusCaller:
 
     def __init__(self, depth, frequency):
         self.min_depth_of_coverage = depth
         self.min_base_frequency = frequency
+
+    # calls consensus on list of piles
+    def call(self, piles):
+        total_length = 0
+        counts = {'A': 0, 'C': 0, 'T': 0, 'G': 0}
+        for pile in piles:
+            if len(pile) < self.min_depth_of_coverage:
+                return None
+            total_length += len(pile)
+            for base in pile:
+                if base == 'A' or base == 'a':
+                    counts['A'] += 1
+                elif base == 'C' or base == 'c':
+                    counts['C'] += 1
+                elif base == 'T' or base == 't':
+                    counts['T'] += 1
+                elif base == 'G' or base == 'g':
+                    counts['G'] += 1
+        ref_base = max(counts.iterkeys(), key=(lambda key: counts[key]))
+        if (float(counts[ref_base]) / total_length) >= self.min_base_frequency:
+            return ref_base
+        else:
+            return None
